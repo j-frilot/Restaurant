7d057dc27ecb3f1e02b8705996e7eed29fe6d189
j-frilot <frilotdr@gmail.com>
2021-01-15 13:29:42 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let slider = document.querySelector(".slider");
+
+$(document).ready(function(){
+    $('.slider').slider();
+});
+
+let indicators = document.querySelector(".indicators")
+$(document).ready(function(){
+    $('.indicators').slider(false);
+});
+
+
+
+let carousel = document.querySelector(".carousel")
+$(document).ready(function () {
+    $(".carousel").carousel("fullWidth");
+    $(".carousel").carousel("fullWidth", true);
+  });
+
+
+
