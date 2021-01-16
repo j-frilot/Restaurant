cd5a5ba4700c0bc49dc1b8ab64f66e82af44dffb
j-frilot <frilotdr@gmail.com>
2021-01-15 18:40:41 -0600

    javascript error

--- js/app.js
+++ js/app.js
@@ -1,21 +1,30 @@
-let slider = document.querySelector(".slider");
 
-$(document).ready(function(){
-    $('.slider').slider();
-});
-
-let indicators = document.querySelector(".indicators")
-$(document).ready(function(){
-    $('.indicators').slider(false);
-});
+let currentSlide = document.querySelector(".slides");
 
 
+let slides = [
+    "url('https://res.cloudinary.com/dsip2gx3i/image/upload/v1600388811/Restaurant/slide1_hmlax3.jpg')", 
+    "url('https://res.cloudinary.com/dsip2gx3i/image/upload/v1600388811/Restaurant/slide2_drujci.jpg')", 
+    "url('https://res.cloudinary.com/dsip2gx3i/image/upload/v1600388811/Restaurant/slide3_dnss6z.jpg')",
+];
 
-// let carousel = document.querySelector(".carousel")
-// $(document).ready(function () {
-//     $(".carousel").carousel("fullWidth");
-//     $(".carousel").carousel("fullWidth", true);
-//   });
+let picOne = slides[0];
+let picTwo = slides[1];
+let picThree = slides[2];
 
+slideCount = 0
+function autoSlide () {
 
+    // left.style.display = "none";
+    // right.style.display = "none";
 
+    slideImage = currentSlide.style.backgroundImage = slides[slideCount]
+    slideCount ++;
+
+    if ( slideCount >= slides.length) {
+        slideCount = 0;
+    }
+
+    console.log(slideCount);
+}
+setInterval(autoSlide, 3000);
